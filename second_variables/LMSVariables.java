4372740ca242af9bdbf73dbc663ad1b1fe707d91
Sureshsai09 <sureshsai6666@gmail.com>
2025-07-30 22:39:27 +0530

    18 july-variables

---
+++ second_variables/LMSVariables.java
@@ -0,0 +1,21 @@
+public class LMSVariables {
+    public static void main(String[] args) {
+        short studentID=101;
+        String studentName="John";
+        String studentEmail="john1@gmail.com";
+        String studentPassword="john123";
+        char student_Gender_1='M';
+        boolean isStudentMale=true;
+        long studentPhone=1234567890;
+
+
+
+        System.out.println("Student ID:"+studentID);
+        System.out.println("Student Name:"+studentName);
+        System.out.println("Student Email:"+studentEmail);
+        System.out.println("Student mobileNo:"+studentPhone);
+        System.out.println("Student Gender:"+student_Gender_1);
+        System.out.println("Student is Male?:"+isStudentMale);
+    }
+    
+}
---
+++ second_variables/VariablesDemo.java
@@ -0,0 +1,28 @@
+public class VariablesDemo {
+    //brand
+   //des
+   //price
+   //datatype vnae=value;
+        static int someData;
+        static int someData2;
+        int price=350;
+        char textbrand='h';
+        String brands="hyliender";
+        float ratings=4.2f;
+        double ratyings_new=4.2;
+
+
+    public static void main(String[] args) {
+        int someDataNew1=10;//initialization
+        int someDataNew2;              //declaration
+        System.out.println(someDataNew1);
+        //
+
+
+        //work with instance variable
+        System.out.println(someData);
+
+
+}
+
+}
