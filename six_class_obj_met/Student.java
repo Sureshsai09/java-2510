61af72b7bfd7f5e2c6add2a59f98045edacdb082
Sureshsai09 <sureshsai6666@gmail.com>
2025-08-01 12:27:00 +0530

    control statements

--- six_class_obj_met/Student.java
+++ six_class_obj_met/Student.java
@@ -1,72 +1,74 @@
 package six_class_obj_met;
+
 import java.util.Scanner;
+
 public class Student {
+    // instance variables
     int studentID;
     String studentName;
-    int totalSessionsAttented;
+    int totalSessionsAttended;
     int attendanceCredits;
     int performanceCredits;
-    int scoreCredits;
     int totalCredits;
     int trainerRating;
-    
 
+    // display student info
+    public void studentInfo() {
+        System.out.println("====== STUDENT INFO======");
+        System.out.println("Student ID: "+studentID);
+        System.out.println("Student Name: "+studentName);
+    }
 
+    // For Taking inputs
+    Scanner sc = new Scanner((System.in));
 
-
-    //display student info
-    public void displayStudentInfo() {
-        System.out.println("======STUDENYT INFO======");
-        System.out.println("Student ID: " + studentID);
-        System.out.println("Student Name: " + studentName);
-
-
-        //for taking inputs
-        Scanner scanner = new Scanner((System.in));
-
-
-        //calculating attendance credits
-        public int sessionsCredits(){
-                System.out.println("total sessions attended: ");
-            totalSessionsAttented = sc.nextInt();
-            if (totalSessionsAttented >= 30) {
-                return attendanceCredits += 5;
-            }
-            else if(totalSessionsAttented>=20){
-                return attendanceCredits += 3;
-            }
-            else{
-                return 0;
-            }
-            }
-
-
-    //calculating performance credits-->with inputs
-    //score is a parameter i.e used as local
-    public int performanceCredits(int score){
-        if (score >= 85) {
-            return performanceCredits += 5;
-            }
-            else if(score>=60){
-                return performanceCredits += 3;
-                }
-                else{
-                    return 0;
-
-
-
-                }
-
-
+    // calculating attendance credits -> no input
+    public int sessionsCredits() {
+        System.out.println("Enter Total Sessions Attended: ");
+        totalSessionsAttended = sc.nextInt();
+        if (totalSessionsAttended>=30) {
+            return attendanceCredits += 5;
+        } else if (totalSessionsAttended>=20) {
+            return attendanceCredits += 3;
+        } else {
+            return 0;
         }
-        //calculating achievements status
-        public void achievementsStatus(){
-            totalCredits=sessionsCredits() + scoreCredits(75);
-            
+    }
 
+    // calculating performance credits -> with input
+    // score is a parameter i.e used as local
+    public int scoreCredits(int score) {
+        if (score >=85) {
+            return performanceCredits += 5;
+        } else if(score >=60) {
+            return performanceCredits += 3;
+        } else {
+            return 0;
+        }
+    }
 
+    // calculating achievement status
+    public void achievementStatus() {
+        totalCredits = sessionsCredits() + scoreCredits(95);
+        if (totalCredits>=10) {
+            System.out.println("==GOLD==");
+        } else if (totalCredits>=8) {
+            System.out.println("==SILVER==");
+        } else {
+            System.out.println("==NEED IMPROVEMENT==");
+        }
+    }
 
+    // assign trainer rating
+    public int trainerRatings() {
+        System.out.println("Enter Trainer Rating: (1-5)");
+        trainerRating = sc.nextInt();
 
-
+        if (trainerRating==5) {
+            return 5000;
+        } else {
+            return 0;
+        }
+    }
 
 }
