18a7f66a6ebf2017b4b8c52ab9c7858b06f51c4a
suresh09 <sureshsai6666@gmail.com>
2025-11-13 00:26:45 +0530

    Strings and literals

---
+++ nine_strings/Demo.java
@@ -0,0 +1,25 @@
+package nine_strings;
+
+public class Demo {
+    public static void main(String[] args) {
+        String str1 = "Hello, ";
+        String str2 = "World!";
+
+        System.out.println(str1);
+        System.out.println(str2);
+        
+
+        //using regular clases
+        Demo demo = new Demo();
+        System.out.println(demo);
+
+        String newName=new String();
+        System.out.println(newName);//print sstring with null value
+
+        String email=new String("krish@gmail.com");
+        System.out.println(email);
+
+    
+    }
+
+}
